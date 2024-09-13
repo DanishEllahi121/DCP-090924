ba1816ce6e05e5315348bdf27bc3052ffc825554
Danish Ellahi <danishelahi8899@gmail.com>
2024-09-13 06:28:06 +0530

    Add max_of_two.c

---
+++ max_of_two.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, max;
+    printf("Enter the two numbers to check maximum: ");
+    scanf("%d %d", &num1, &num2);
+    max = (num1 > num2) ? num1 : num2;
+    printf("The Maximum of %d and %d is: %d\n", num1, num2, max);
+    return 0;
+}
