c2104ba579fd9b64c11a58d6f31650f70803c6b9
Danish Ellahi <danishelahi8899@gmail.com>
2024-09-17 06:49:28 +0530

    Add palindrome_check.c

---
+++ palindrome_check.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void palin_check(int num);
+int main()
+{
+    int num;
+    printf("Enter the Number to check palindrome: ");
+    scanf("%d", &num);
+    palin_check(num);
+    return 0;
+}
+void palin_check(int num)
+{
+    int remainder, original, reversed = 0;
+    original = num;
+    while (num > 0)
+    {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num = num / 10;
+    }
+    if (original = reversed)
+    {
+        printf("The %d number is palindrome %d\n", original, reversed);
+    }
+    else
+    {
+        printf("The %d number is not palindrome %d\n", original, reversed);
+    }
+}
